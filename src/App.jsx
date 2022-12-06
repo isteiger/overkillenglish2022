93fee3b4def361241ea1232aa1eac6a6374f60ec
Ian Steiger <iksteiger@gmail.com>
2022-12-06 13:53:33 -0500

    jaslkfjf

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import "./App.css";
+
+function App() {
+    return (
+        <div>
+            <div style={{ display: "flex", flexDirection: "row" }}>
+                <div style={{ backgroundColor: "lightblue", height: "85vh", width: "50vw", position: "relative" }}>
+                    <Typography variant="h1" fontWeight="bold" color="rgba(255,255,255,0.8)" style={{ position: "absolute", bottom: 0, right: 15, textAlign: "end", fontFamily: "Merriweather, serif", fontSize: "8rem" }}>
+                        <span className='authorText1'>MARTIN<br /></span>
+
+                        KAPEL
+                    </Typography>
+                    <img src="./images/martin.png" />
+                </div>
+
+
+
+                <div style={{ backgroundColor: "rgb(60, 119, 123)", height: "85vh", width: "50vw", position: "relative" }}>
+                    <Typography variant="h1" fontWeight="bold" color="rgba(255,255,255,0.8)" style={{ position: "absolute", bottom: 0, left: 15, fontFamily: "Merriweather, serif", fontSize: "8rem" }}>
+                        <span className='authorText1'>ELIE<br /></span>
+                        WIESEL
+                    </Typography>
+                </div>
+            </div>
+            <div style={{ height: "15vh", display: "flex", alignItems: "center", justifyContent: "center" }}>
+                <Typography variant="h2" component="div" fontWeight="bold">
+                    Holocaust Survivors
+                </Typography>
+            </div>
+        </div>
+
+    );
+}
+
+export default App;
