ad26a6c6d61a59e847141ab4f08ca6d980221838
Ian Steiger <iksteiger@gmail.com>
2022-12-06 13:58:30 -0500

    asdfdsfs

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,5 @@
 import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
 import "./App.css";
 
 function App() {
@@ -18,7 +12,7 @@ function App() {
 
                         KAPEL
                     </Typography>
-                    <img src="./images/martin.png" />
+                    <img src="./images/martin.png" alt="martin kapel" />
                 </div>
 
 
