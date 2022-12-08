fc57428d1fd4835e2c2a78f7263b8394e25c8ded
Ian Steiger <iksteiger@gmail.com>
2022-12-08 12:53:22 -0500

    sfsdfdasfds

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,7 @@ import * as React from 'react';
 import Typography from '@mui/material/Typography';
 import "./App.css";
 import Timeline from '@mui/lab/Timeline';
-import TimelineItem from '@mui/lab/TimelineItem';
-import TimelineSeparator from '@mui/lab/TimelineSeparator';
-import TimelineConnector from '@mui/lab/TimelineConnector';
-import TimelineContent from '@mui/lab/TimelineContent';
-import TimelineDot from '@mui/lab/TimelineDot';
-import { Card, CardHeader, CardMedia } from '@mui/material';
+import { Card } from '@mui/material';
 import TimelineElement from './TimelineElement';
 
 function App() {
