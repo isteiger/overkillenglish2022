c5e74e5fa8f631240fc3252c87486a917f02ec7f
Ian Steiger <iksteiger@gmail.com>
2023-01-18 16:48:09 -0500

    fdfgdfsg

--- src/InfoElement.jsx
+++ src/InfoElement.jsx
@@ -1,4 +1,4 @@
-import { Card, CardHeader, CardMedia, Divider, Typography } from "@mui/material";
+import { Card, CardHeader, Divider, Typography } from "@mui/material";
 
 export default function InfoElement(props) {
 
@@ -10,11 +10,11 @@ export default function InfoElement(props) {
                 <div style={{ width: "40%" }} >
                     {props.ksubheader ? <CardHeader title="Martin Kapel" subheader={props.ksubheader} style={{ textAlign: "right" }} /> : <CardHeader title="Martin Kapel" style={{ textAlign: "right" }} />}
 
-                    <div style={{ display: "flex", height: "inherit" }}>
-                        <Typography variant="body2" color="text.secondary" style={{ marginLeft: "1rem", flexGrow: "1", textAlign: "right" }}>
+                    <div style={{ display: "flex", height: "100%" }}>
+                        <Typography variant="body2" color="text.secondary" style={{ paddingBottom: "16px", marginLeft: "1rem", flexGrow: "1", textAlign: "right" }}>
                             {props.kdescription}
                         </Typography>
-                        <div style={{ marginLeft: "1rem", borderTopLeftRadius: "1rem", flexBasis: "20vw", flexGrow: 0, flexShrink: 0, backgroundImage: `url('${props.kimg}')`, backgroundSize: "cover" }}>
+                        <div style={{ backgroundPosition: "center", marginLeft: "1rem", borderTopLeftRadius: "1rem", flexBasis: "20vw", flexGrow: 0, flexShrink: 0, backgroundImage: `url('${props.kimg}')`, backgroundSize: "cover" }}>
 
                         </div>
                         {/*
@@ -30,7 +30,7 @@ export default function InfoElement(props) {
 
                 <div style={{ width: "20%", borderRight: "thin solid rgba(0, 0, 0, 0.12)", borderLeft: "thin solid rgba(0, 0, 0, 0.12)" }}>
                     <CardHeader title="Similarities" style={{ textAlign: "center" }} />
-                    <Typography variant="body2" color="text.secondary" style={{ marginRight: "1rem", textAlign: "center" }}>
+                    <Typography variant="body2" color="text.secondary" style={{ marginLeft: "16px", marginRight: "16px", textAlign: "center" }}>
                         {props.similarities}
                     </Typography>
                 </div>
@@ -38,12 +38,12 @@ export default function InfoElement(props) {
 
                 <div style={{ width: "40%" }}>
                     {props.wsubheader ? <CardHeader title="Elie Wiesel" subheader={props.wsubheader} /> : <CardHeader title="Elie Wiesel" />}
-                    <div style={{ display: "flex", height: "inherit" }}>
-                        <div style={{ marginRight: "1rem", borderTopRightRadius: "1rem", flexBasis: "20vw", flexGrow: 0, flexShrink: 0, backgroundImage: `url('${props.wimg}')`, backgroundSize: "cover" }}>
+                    <div style={{ display: "flex", height: "100%" }}>
+                        <div style={{ backgroundPosition: "center", marginRight: "1rem", borderTopRightRadius: "1rem", flexBasis: "20vw", flexGrow: 0, flexShrink: 0, backgroundImage: `url('${props.wimg}')`, backgroundSize: "cover" }}>
 
                         </div>
 
-                        <Typography variant="body2" color="text.secondary" style={{ marginRight: "1rem" }}>
+                        <Typography variant="body2" color="text.secondary" style={{ marginRight: "1rem", paddingBottom: "16px" }}>
                             {props.wdescription}
                         </Typography>
                     </div>
